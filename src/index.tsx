9dc0ae090689006dc43eace08b9ebe05b1ce96f4
sualko <klaus@jsxc.org>
2020-04-21 13:54:25 +0200

    abort in unsecure env

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,12 @@ let bodyElement = document.getElementsByTagName('body')[0];
 bodyElement.classList.add('jsxc-fullscreen');
 bodyElement.classList.add('jsxc-two-columns');
 
+if (window.location.hostname && window.location.hostname !== 'localhost') {
+    alert('This application stores passwords in the local storage. This should not be done on a website.');
+
+    throw new Error('Unsecure enviornment');
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
